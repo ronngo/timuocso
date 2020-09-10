a0326be6a90d57a5f2389b3a59d95c94cfdef745
ronngo <70925598+ronngo@users.noreply.github.com>
2020-09-10 21:44:18 +0700

    Add files via upload

---
+++ timuocso/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace uocchunglonnhat
+{
+    class Program
+    {
+        static void timuocso()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            int i, n1, n2, j, uscln = 1;
+            Console.Write("Nhập a: ");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhập b: ");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            j = (n1 < n2) ? n1 : n2;
+            for (i = 1; i <= j; i++)
+            {
+                if (n1 % i == 0 && n2 % i == 0)
+                {
+                    uscln = i;
+                }
+            }
+            Console.Write("\nƯớc số chung lớn nhất của {0} và {1} là: {2}\n\n", n1, n2, uscln);
+        }
+        static void Main(string[] args)
+        {
+
+            timuocso();
+            Console.ReadKey();
+        }
+    }
+}
+
